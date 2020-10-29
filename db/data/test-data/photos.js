1536f20d1dd3fd53dc05241255e59ba8aec5d0f7
JFParrott <jfparrott@protonmail.com>
2020-10-29 20:55:54 +0000

    change data url key name and run seed:prod

--- db/data/test-data/photos.js
+++ db/data/test-data/photos.js
@@ -1,62 +1,62 @@
 module.exports = [
   {
     location: 'Mars',
-    'url-tag': 'Mars_test_1.jpg',
+    url_tag: 'Mars_test_1.jpg',
     label: 'Mars 1',
   },
   {
     location: 'Mars',
-    'url-tag': 'Mars_test_2.jpg',
+    url_tag: 'Mars_test_2.jpg',
     label: 'Mars 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_!.jpg',
+    url_tag: 'Venus_test_!.jpg',
     label: 'Venus',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_2.jpg',
+    url_tag: 'Venus_test_2.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_3.jpg',
+    url_tag: 'Venus_test_3.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_4.jpg',
+    url_tag: 'Venus_test_4.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_5.jpg',
+    url_tag: 'Venus_test_5.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_6.jpg',
+    url_tag: 'Venus_test_6.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Venus',
-    'url-tag': 'Venus_test_7.jpg',
+    url_tag: 'Venus_test_7.jpg',
     label: 'Venus 2',
   },
   {
     location: 'Jupiter',
-    'url-tag': 'Jupiter_test_1.jpg',
+    url_tag: 'Jupiter_test_1.jpg',
     label: 'Jupiter',
   },
   {
     location: 'Saturn',
-    'url-tag': 'Saturn_test_1.jpeg',
+    url_tag: 'Saturn_test_1.jpeg',
     label: 'Saturn 1',
   },
   {
     location: 'Saturn',
-    'url-tag': 'Saturn_test_2.jpg',
+    url_tag: 'Saturn_test_2.jpg',
     label: 'Saturn 2',
   },
 ];
