dd31f78fb24ff8024b0535aca0c0f2265447e89d
JFParrott <jfparrott@protonmail.com>
2021-01-18 13:56:27 +0000

    add resized image tags and alter migrations

--- db/migrations/20201025151453_create_photos_table.js
+++ db/migrations/20201025151453_create_photos_table.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('photos', (photosTable) => {
     photosTable.increments('photo_id');
     photosTable.string('url_tag').notNullable();
+    photosTable.string('resized').notNullable();
     photosTable.string('location').references('locations.name').notNullable();
     photosTable.string('label').notNullable();
   });
